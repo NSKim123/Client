9f31b25dae253c8950593de0e0b9f8f16ad17ff5
kmw7691 <101631103+JeongEunChang@users.noreply.github.com>
2022-10-21 19:51:30 +0900

    week 6-5
    operator overload
    copy constructor
    copy assignment operator

---
+++ C_and_CPP/CPP/cpp_copyfunction/cpp_copyfunction.cpp
@@ -0,0 +1,156 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+/*
+
+    기본 생성자
+    소멸자
+    복사 생성자
+    복사 대입 연산자
+
+    등은 명시적으로 만들지 않으면
+    컴파일러에 의해서 자동으로 만들어진다.
+
+
+    복사 함수 : 복사 생성자 copy constructor, 복사 대입 연산자 copy assignment 등을 복사함수라고 한다.
+
+
+*/
+
+class CWeapon
+{
+private:
+    int mAP = 0;
+};
+
+class CSlime
+{
+//private:
+public:
+    int mX = 0;
+    int mHP = 0;
+    int mAP = 0;
+
+    CWeapon* mpWeapon = nullptr;    //무기 객체 참조
+
+public:
+    CSlime();
+    ~CSlime();
+
+    CSlime(const CSlime& t);         //복사 생성자
+   
+    void operator=(const CSlime& t); //복사 대입 연산자
+};
+
+class CSlimeBoss : public CSlime
+{
+public:
+    CSlimeBoss();
+    ~CSlimeBoss();
+
+    CSlimeBoss(const CSlimeBoss& t);
+
+    void operator=(const CSlimeBoss& t);
+};
+
+
+int main()
+{
+    CSlime tSlimeA;     //기본 생성자가 호출
+    CSlime tSlimeB;     //기본 생성자가 호출
+
+    tSlimeA.mX = 2;
+    tSlimeA.mHP = 100;
+    tSlimeA.mAP = 3;
+
+    CSlime tSlimeC = tSlimeA;    //'복사 생성자'가 호출
+
+    tSlimeB = tSlimeA;      //'복사 대입 연산자'가 호출
+
+
+    return 0;
+}
+
+CSlime::CSlime()
+    :mX(0), mHP(0), mAP(0),mpWeapon(nullptr)
+{
+    mpWeapon = new CWeapon;
+}
+CSlime::~CSlime()
+{
+    if (nullptr != mpWeapon)
+    {
+        delete mpWeapon;
+        mpWeapon = nullptr;
+    }
+}
+
+CSlime::CSlime(const CSlime& t)         //복사 생성자
+{
+    //얕은 복사 shallow copy   (곧이곧대로 복사)
+
+    /*this->mX = t.mX;
+    mHP = t.mHP;
+    mAP = t.mAP;
+
+    mpWeapon = t.mpWeapon;*/
+
+    //깊은 복사 deep copy
+    this->mX = t.mX;
+    mHP = t.mHP;
+    mAP = t.mAP;
+
+    //새롭게 CWeapon타입의 객체 생성
+    mpWeapon = new CWeapon();
+    //CWeapon의 복사대입연산자가 작동
+    *mpWeapon = *t.mpWeapon;
+}
+void CSlime::operator=(const CSlime& t)   //복사 대입 연산자
+{
+    //얕은 복사 shallow copy
+    /*this->mX = t.mX;
+    mHP = t.mHP;
+    mAP = t.mAP;
+
+    mpWeapon = t.mpWeapon;*/
+
+    //깊은 복사 deep copy
+    this->mX = t.mX;
+    mHP = t.mHP;
+    mAP = t.mAP;
+
+    //무기가 없는 경우
+    if (nullptr == mpWeapon)
+    {
+        //새롭게 CWeapon타입의 객체 생성
+        mpWeapon = new CWeapon();
+        //CWeapon의 복사대입연산자가 작동
+        *mpWeapon = *t.mpWeapon;
+    }
+    else //무기가 있는 경우
+    {
+        //CWeapon의 복사대입연산자가 작동
+        *mpWeapon = *t.mpWeapon;
+    }
+}
+
+CSlimeBoss::CSlimeBoss()
+{
+
+}
+CSlimeBoss::~CSlimeBoss()
+{
+
+}
+
+CSlimeBoss::CSlimeBoss(const CSlimeBoss& t)
+{
+
+}
+
+void CSlimeBoss::operator=(const CSlimeBoss& t)
+{
+
+}
---
+++ C_and_CPP/CPP/cpp_operator_overload/cpp_operator_overload.cpp
@@ -0,0 +1,113 @@
+﻿/*
+    프로그래밍 언어의 가장 기본적인 구성요소 3가지
+        타입 Data Type
+        연산자 operator
+        제어구조 flow control
+
+
+
+    연산자 오버로드 operator overload
+        (C++에서)  클래스(사용자정의타입)에 대해 연산자를 중복해서 정의하는 것.
+
+
+        함수의 오버로드가 '함수의 이름은 같고 매개변수 리스트가 다른 것'처럼
+        연산자 오버로드는 그 '연산자의 형태는 같고 그 연산가 다루는 타입들'이 달라진다
+
+
+
+
+    사용법: 'operator예약어 뒤에 해당 연산자를 붙이고' 나서 함수의 정의를 구현을 하면 된다.
+
+
+    장점:
+    1)해당 연산자 표기의 의미가 유지된다.
+    2)간략한 표기가 가능하다.
+
+    단점:
+    1)구현 난이도가 있다.
+
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+//prealgebra 산수
+//algebra 대수학
+//geometry 기하학
+
+class CRyuAlgebra
+{
+private:
+    int mVal = 0;
+
+public:
+    //매개변수 있는 생성자
+    CRyuAlgebra(int tVal)
+        :mVal(tVal)   //초기화 목록
+    {
+        //mVal = tVal;   <-- 이것과 같은 의미이며, 이것과 연산이 더 빠르다.
+    }
+    ~CRyuAlgebra(){}
+
+    void operator=(const CRyuAlgebra& t)
+    {
+        this->mVal = t.mVal;
+    }
+
+    CRyuAlgebra operator+(const CRyuAlgebra& t)
+    {
+        return this->mVal * 10 + t.mVal;
+    }
+
+    int GetVal()
+    {
+        return mVal;
+    }
+
+};
+
+
+//전역으로 만든 연산자 오버로드
+ostream& operator<<(ostream& tOS, CRyuAlgebra& t)
+{
+    tOS << t.GetVal();
+
+    return tOS;
+}
+
+
+int main()
+{
+    /*
+    int tA = 3;
+    int tB = 2;
+    int tResult = 0;
+
+    tResult = tA + tB;
+    */
+
+
+    /*CRyuAlgebra tA(1);
+    CRyuAlgebra tB(2);
+    CRyuAlgebra tResult(0);*/
+
+    //이 코드들은 매개변수 있는 생성자로 컴파일됨
+    //(변수선언의 초기화 구문의 =은 일반적인 대입연산자가 아니다.)
+    CRyuAlgebra tA = 1;
+    CRyuAlgebra tB = 2;
+    CRyuAlgebra tResult = 0;
+
+    tResult = tA + tB;  //여기의 =은 대입연산자이다.
+
+    //cout << tResult.GetVal() << endl;
+    cout << tResult << endl;
+
+    
+
+
+    return 0;
+}
+
+
