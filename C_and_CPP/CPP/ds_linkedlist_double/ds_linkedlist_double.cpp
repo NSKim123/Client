0b4b755ddb01f4c49e77cb3e1adf2c3406867234
kmw7691 <101631103+JeongEunChang@users.noreply.github.com>
2022-11-02 19:51:34 +0900

    week 8-3
    simple linked list
    double linked list

---
+++ C_and_CPP/CPP/ds_linkedlist_double/ds_linkedlist_double.cpp
@@ -0,0 +1,129 @@
+﻿
+/*
+    double linked list
+    양방향 연결 리스트, 이중 연결 리스트
+
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+struct SNode
+{
+    int mData = 0;
+
+    SNode* mpPrev = nullptr;     //tail -----> head
+    SNode* mpNext = nullptr;     //head -----> tail
+};
+
+SNode* mpHead = nullptr;
+SNode* mpTail = nullptr;
+
+//double 링크드리스트의 기본구조 만들기
+void InitList()
+{
+    mpHead = new SNode();
+    mpTail = new SNode();
+
+
+    //링크 조작
+    mpHead->mpPrev = mpHead;
+    mpHead->mpNext = mpTail;
+
+    mpTail->mpPrev = mpHead;
+    mpTail->mpNext = mpTail;
+}
+
+SNode* OrderedInsert(int k)
+{
+    SNode* s = nullptr;
+    SNode* i = nullptr;
+
+    s = mpHead->mpNext;
+
+    //추가할 위치 선정
+    while (s->mData <= k && s != mpTail)
+    {
+        s = s->mpNext;
+    }
+
+    i = new SNode();
+    i->mData = k;
+    
+    //링크 조작
+    s->mpPrev->mpNext = i;
+    i->mpPrev = s->mpPrev;
+    s->mpPrev = i;
+    i->mpNext = s;
+
+    return i;
+}
+
+SNode* FindNode(int k)
+{
+    SNode* s = nullptr;
+
+    s = mpHead->mpNext;
+
+    while (s->mData != k && s != mpTail)
+    {
+        s = s->mpNext;
+    }
+
+    return s;
+}
+
+
+bool DeleteNode(int k)
+{
+    SNode* s = nullptr;
+
+    s = FindNode(k);
+
+    if (s != mpTail)
+    {
+        s->mpPrev->mpNext = s->mpNext;
+        s->mpNext->mpPrev = s->mpPrev;
+
+        delete s;
+
+        return true;
+    }
+    
+    
+    return false;
+    
+
+}
+
+void DisplayList(SNode* t)
+{
+    while (t != mpTail)
+    {
+        cout << "\t" << t->mData;
+        t = t->mpNext;
+    }
+}
+
+
+int main()
+{
+    InitList();
+
+    OrderedInsert(3);
+    OrderedInsert(1);
+    OrderedInsert(10);
+
+    DisplayList(mpHead->mpNext);
+
+    cout << endl;
+    DeleteNode(3);
+
+    DisplayList(mpHead->mpNext);
+
+    return 0;
+}
+
+
--- C_and_CPP/CPP/ds_linkedlist_simple/ds_linkedlist_simple.cpp
+++ C_and_CPP/CPP/ds_linkedlist_simple/ds_linkedlist_simple.cpp
@@ -3,6 +3,16 @@
 #include <iostream>
 using namespace std;
 
+//자료구조 data structure : 자료들을 담아두는 구조물, 구조와 특성
+//알고리즘 algorithm: 임의의 '문제를 해결'하기 위한 '논리적인' '절차'의 명세(솔루션)
+
+//배열 : '동일한 타입'의 '원소들'의 (물리적으로) '연속적인' 메모리 블럭
+
+//링크드 리스트 linked list
+//      :'노드'가 '데이터'와 '링크'를 가지고, 각각의 링크에 의해 '한줄로(선형으로)' '연결'되어 있는 자료구조
+// 
+//simple linked list 단순 연결 링크드 리스트 ( 단방향 연결 링크드 리스트, 심플링크드리스트)
+
 //노드 를 정의
 struct SNode
 {
@@ -19,13 +29,116 @@ SNode* mpHead = nullptr;
 SNode* mpTail = nullptr;
 
 
-//링크드리스트의 기본구조를 만들자
+//편의를 위해서 선언, 정의 합쳐서 쓰겠다.
+//심플링크드리스트의 기본구조를 만들자
+void InitList()
+{
+    //head 노드, tail 노드 확보
+    mpHead = new SNode();
+    mpTail = new SNode();
+
+    //링크 조작
+    mpHead->mpNext = mpTail;
+    mpTail->mpNext = mpTail;
+}
+
+//추가, 삭제, 탐색
+
+//정렬하면서 추가(10, 5를 예로 들어보자)
+SNode* OrderedInsert(int k)
+{
+    SNode* p = nullptr;
+    SNode* s = nullptr;
+
+    SNode* r = nullptr;
+
+    p = mpHead;
+    s = p->mpNext;
+    
+    while (s->mData <= k && s != mpTail)
+    {
+        p = p->mpNext;
+        s = p->mpNext;
+    }
+
+    r = new SNode();
+    r->mData = k;
+    p->mpNext = r;
+    r->mpNext = s;
+
+    return r;
+}
+
+//임의의 노드의 주소값을 받아서 그 노드의 다음 노드 삭제
+bool DeleteNext(SNode* t)
+{
+    SNode* tpNode = nullptr;
+    if (t->mpNext == mpTail)
+    {
+        return false;
+    }
+
+    tpNode = t->mpNext;
+    t->mpNext = t->mpNext->mpNext;
+
+    delete tpNode;
+
+    return true;
+}
+
+SNode* FindNode(int k)
+{
+    SNode* tpNode = nullptr;
+
+    tpNode = mpHead->mpNext;
+    while (tpNode->mData != k && tpNode != mpTail)
+    {
+        tpNode = tpNode->mpNext;
+    }
 
 
+    return tpNode;
+}
+
+void Display(SNode* t)
+{
+    while (t != mpTail)
+    {
+        cout << "\t" << t->mData;
+
+        t = t->mpNext;
+    }
+}
 
 int main()
 {
+    InitList();
 
+    OrderedInsert(10);
+    OrderedInsert(5);
+    OrderedInsert(7);
+    OrderedInsert(1);
+    OrderedInsert(8);
+
+    Display(mpHead->mpNext);
+
+    cout << endl;
+
+    SNode* t = FindNode(7);
+    DeleteNext(t);
+
+    Display(mpHead->mpNext);
+
+    cout << endl;
+
+    SNode* tt = FindNode(1);
+    tt->mData = 777;
+
+    Display(mpHead->mpNext);
+
+    //링크드리스트 삭제구문이 필요하다.(생략한다)
+
+    
 
     return 0;
 }
