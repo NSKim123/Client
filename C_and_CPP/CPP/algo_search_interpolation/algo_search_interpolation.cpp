8799d0928d43026bd5a09591fad40e15edb19d60
kmw7691 <101631103+JeongEunChang@users.noreply.github.com>
2022-11-10 19:53:54 +0900

    week 9-2
    algorithm( time compexity, Big-O )
    sequential search
    interpolation search

---
+++ C_and_CPP/CPP/algo_search_interpolation/algo_search_interpolation.cpp
@@ -0,0 +1,42 @@
+﻿/*
+    이분검색(이진검색) interpolation search
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void DoBinarySearch(int tArray[], int tBegin, int tEnd, int tData)
+{
+    int tIndexMiddle = 0;
+
+    tIndexMiddle = (tBegin + tEnd) / 2;
+
+    if (tArray[tIndexMiddle] == tData)
+    {
+        //base case
+        cout << "검색성공" << endl;
+    }
+    else if(tArray[tIndexMiddle] > tData)
+    {
+        //recursive case
+        DoBinarySearch(tArray, tBegin, tIndexMiddle - 1, tData);
+    }
+    else if(tArray[tIndexMiddle] < tData)
+    {
+        //recursive case
+        DoBinarySearch(tArray, tIndexMiddle + 1, tEnd , tData);
+    }
+    else
+    {
+        //base case
+        cout << "검색실패" << endl;
+    }
+
+}
+
+
+int main()
+{
+    return 0;
+}
