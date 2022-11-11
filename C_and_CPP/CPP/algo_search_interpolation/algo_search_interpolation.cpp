b499f12bfa2c5214ac707868f137912c63b349a9
kmw7691 <101631103+JeongEunChang@users.noreply.github.com>
2022-11-11 19:53:23 +0900

    week9-3
    interpolation search
    hash

--- C_and_CPP/CPP/algo_search_interpolation/algo_search_interpolation.cpp
+++ C_and_CPP/CPP/algo_search_interpolation/algo_search_interpolation.cpp
@@ -1,14 +1,39 @@
 ﻿/*
     이분검색(이진검색) interpolation search
 
+        정렬되어있는 자료들에 대해 이분하여 검색의 범위를 반씩 줄여가며 검색하는 방법
+
+        시간복잡도 : O(log n)
+        공간복잡도 : n
 */
 
 #include <iostream>
 using namespace std;
 
+int gCount = 0;
+
 void DoBinarySearch(int tArray[], int tBegin, int tEnd, int tData)
 {
     int tIndexMiddle = 0;
+
+    gCount++;
+
+    //base case
+    //원소가 1개인 경우 <-- 더이상 이분할 수 없는 상태
+    if (tBegin == tEnd)
+    {
+        if (tData == tArray[tBegin])
+        {
+            cout << "--검색 성공" << endl;
+            cout << "--gCount: " << gCount << endl;
+        }
+        else
+        {
+            cout << "--검색 실패" << endl;
+        }
+
+        return;
+    }
 
     tIndexMiddle = (tBegin + tEnd) / 2;
 
@@ -16,6 +41,7 @@ void DoBinarySearch(int tArray[], int tBegin, int tEnd, int tData)
     {
         //base case
         cout << "검색성공" << endl;
+        cout << "gCount: " << gCount << endl;
     }
     else if(tArray[tIndexMiddle] > tData)
     {
@@ -38,5 +64,12 @@ void DoBinarySearch(int tArray[], int tBegin, int tEnd, int tData)
 
 int main()
 {
+    int tArray[] = { 1,2,8,9,11,19,29 };
+    int tLength = 7;
+
+    int tData = 29;
+    DoBinarySearch(tArray, 0, tLength - 1, tData);
+
+
     return 0;
 }
