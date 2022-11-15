8ff325f0e3bfe351378bbbe18668462b7e5ffb04
kmw7691 <101631103+JeongEunChang@users.noreply.github.com>
2022-11-15 19:51:25 +0900

    week 10-2
    quick sort
    list : stl container

---
+++ C_and_CPP/CPP/algo_sort_quick/algo_sort_quick.cpp
@@ -0,0 +1,111 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+//파티셔닝을 하고 피벗 인덱스를 리턴
+int DoPartition(int tArray[], int tBegin, int tEnd)
+{
+    int tPivot = 0;     //분할의 기준점
+    int L = 0;          //왼쪽 집합의 지시자 인덱스
+    int R = 0;          //오른쪽 집합의 지시자 인덱스
+
+    int tTemp = 0;      //교환을 위한 임시메모리 변수
+
+    L = tBegin;         //<-- 처음에 왼쪽 집합 지시자는 왼쪽 집합의 왼편 끝에서부터 시작한다.
+    R = tEnd;           //<--처음에 오른쪽 집합 지시자는 오른쪽 집합의 오른편 끝에서 시작한다.
+
+    //가운데 위치의 것을 피벗으로 선정
+    tPivot = (tBegin + tEnd) / 2;
+
+    while (L < R)
+    {
+        //적합한 L을 찾는다.
+        while (tArray[L] < tArray[tPivot] && L < R)
+        {
+            L++;
+        }
+        //적합한 R을 찾는다.
+        while (tArray[R] >= tArray[tPivot] && L < R)
+        {
+            R--;
+        }
+        
+        //여기까지 왔다면,
+        //tArray[L] >= tArray[tPivot]   <-- 즉, tArray[L] 이라는 것은 피벗의 오른쪽에 있어야 한다는 것이다.
+        //tArray[R] < tArray[tPivot]    <-- 즉, tArray[R] 이라는 것은 피벗의 왼쪽에 있어야 한다는 것이다.
+
+        if (L < R)
+        {
+            tTemp = tArray[L];
+            tArray[L] = tArray[R];
+            tArray[R] = tTemp;
+
+            if (L == tPivot)
+            {
+                //before
+                //    L                 R
+                //  pivot
+                // 
+                //after
+                //    L                 R
+                //                    pivot
+                tPivot = R;
+            }
+        }
+    }
+
+    //L==R
+    //값 교환 R을 피벗으로 가정한다 (이제 다른 부분집합을 위해서다)
+    tTemp = tArray[tPivot];
+    tArray[tPivot] = tArray[R];
+    tArray[R] = tTemp;
+
+
+
+
+    return R;
+}
+
+void QuickSort(int tArray[], int tBegin, int tEnd)
+{
+    int tPivot = 0;
+
+    //tBegin >= tEnd 라면 우너소가 하나이므로 더 이상 쪼갤 수 없음
+    //tBegin < tEnd 이면 부분집합으로 쪼갤 수 있음
+    if (tBegin < tEnd)
+    {
+        //파티셔닝 partitioning
+        //이 예시에서 pivot은 가운데 위치에 있는 것으로 하겠다.
+        tPivot = DoPartition(tArray, tBegin, tEnd);
+
+        QuickSort(tArray, tBegin, tPivot - 1);
+        QuickSort(tArray, tPivot + 1, tEnd);
+    }
+}
+
+
+int main()
+{
+    int tArray[] = { 69,10,30,16,2 };
+    int tLength = 5;
+
+    for (auto t : tArray)
+    {
+        cout << t << "\t";
+    }
+    cout << endl;
+
+    QuickSort(tArray, 0, tLength - 1);
+
+    for (auto t : tArray)
+    {
+        cout << t << "\t";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
---
+++ C_and_CPP/CPP/cpp_stl_list/cpp_stl_list.cpp
@@ -0,0 +1,89 @@
+﻿
+/*
+    STL Standard Template Library
+
+        구성요소 세가지 
+            container : 자료구조를 일반화하여 범용적으로 만들어놓은 것
+            iterator : container와 algorithm과 같이 사용할 수 있게 만들어진 일반화된 포인터
+            algorithm : 알고리즘을 일반화하여 범용적으로 만들어놓은 것
+
+
+
+
+    STL의 list컨테이너
+
+        더블링크드리스트로 만들어졌다.
+
+*/
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+class CUnit
+{
+private:
+    int mVal = 0;
+
+public:
+    CUnit(int tVal)
+        :mVal(tVal){
+    }
+    
+    void SetVal(int tVal)
+    {
+        mVal = tVal;
+    }
+    int GetVal()
+    {
+        return mVal;
+    }
+};
+
+
+
+int main()
+{
+    /*
+    CUnit으로 다루면 복사되어 CUnit 객체가 새로 만들어지는 것이다. 
+    
+    list<CUnit> tUnitList;
+
+    CUnit tUnit(0);
+    tUnitList.push_back(tUnit);
+    
+    */
+
+    //Cunit* 타입의 객체를 다루는 링크드리스트 선언
+    list<CUnit*> tUnitList;
+
+    CUnit* tpUnit = nullptr;
+
+    //0번 아이디를 가진 유닛
+    tpUnit = nullptr;
+    tpUnit = new CUnit(0);
+
+    tUnitList.push_back(tpUnit);
+
+    //1번 아이디를 가진 유닛
+    tpUnit = nullptr;
+    tpUnit = new CUnit(1);
+
+    tUnitList.push_back(tpUnit);
+
+    //2번 아이디를 가진 유닛
+    tpUnit = nullptr;
+    tpUnit = new CUnit(2);
+
+    tUnitList.push_back(tpUnit);
+
+    list<CUnit*>::iterator tItor;
+    for (tItor = tUnitList.begin(); tItor != tUnitList.end(); ++tItor)
+    {
+        cout << "원소의 값: " << (*tItor)->GetVal() << endl;
+    }                            //iterator 도 포인터 변수이기 때문에 두번 참조해야한다.
+
+
+
+    return 0;
+}
